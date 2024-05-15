24e8c8c0bb4b75576c5fb49bd93e7b652d3eb965
madaz1movsa <kutbidinovdanislan9@gmail.com>
2024-05-15 14:08:43 +0600

    Построили Логику игры 'Камень Ножницы Бумага

---
+++ telebot_game.py
@@ -0,0 +1,34 @@
+import random
+import telebot
+
+
+bot = telebot.TeleBot("7049222427:AAFZHOejH7JfXx_SQZrD5UUCBDLDZkDBXog")
+
+
+@bot.message_handler(commands=["Game"])
+def send_welcome(message):
+    bot.reply_to(message, "Приветсвую Вас в игре 'Камень Ножницы Бумага!' Или же 'Рошамбо'"
+                          "Выберите камень (к), ножницы (н), или бумагу (б)")
+
+
+@bot.message_handler(func=lambda message: True)
+def play_game(message):
+    user_move = message.text
+    if user_move not in ["к", "н", "б"]:
+        bot.reply_to(message, "Пожалуйста введите либо к, н, или б.")
+    else:
+        computer_move = random.choice(["к", "н", "б"])
+        if (user_move == "к" and computer_move == "н") or \
+                (user_move == "н" and computer_move == "б") or \
+                (user_move == "б" and computer_move == "к"):
+            bot.reply_to(message, "Ура! Вы выиграли!")
+        elif user_move == computer_move:
+            bot.reply_to(message, "Вывод Ничья!")
+        else:
+            bot.reply_to(message, "Вы проиграли Боту!")
+
+
+bot.infinity_polling()
+
+
+
